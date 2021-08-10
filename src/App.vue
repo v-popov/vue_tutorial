3b5ca4721fc1255c30faa2708a045847b3aec0b8
victor <victorvtf@gmail.com>
2021-08-09 19:00:38 -0700

    first working example

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,50 @@
+// This is Single File Component
+// It should have template, script, and style sections
+
+// Specifying layout - buttons, titles, etc.
+// {{ }} -> interpolation
+// v-on - директива; it can subscribe to different events (click in our case)
+// v-on for shorthand can be replaced with "@" symbol (addDislike example)
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+
+    <div>
+      <button v-on:click="addLike">Like</button>
+      <button @click="addDislike">Dislike</button>
+    </div>
+
+    <div>Num Likes: {{ likes }}</div>
+    <div>Num DisLikes: {{ dislikes }}</div>
+
   </div>
-  <router-view/>
 </template>
 
+
+// Specifying component logic; create functions; declare variables
+// In this section we need to export an object: export default { }
+<script>
+export default {
+
+  data() { // function
+    return {
+      likes: 0, // initializing variable ("field") likes
+      dislikes: 8,
+    }
+  },
+
+  methods: {
+    addLike() {
+      this.likes += 1;
+    },
+    addDislike() {
+      this.dislikes += 1;
+    }
+  }
+}
+</script>
+
+
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+// We can create our component as a regular JS object:
+// const App = {
+//   data,
+//   methods
+// }
+
+// Or alternatively we can create it as a Single File Component: src/App.vue
+// and import it as follows:
+import App from './App'
+
+
+
+
+createApp(App).mount('#app') // there is a div with "app" id in index.html
