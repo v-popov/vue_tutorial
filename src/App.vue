84c68d14f7d84c5f095dc0071b5ffc76acc10296
victor <victorvtf@gmail.com>
2021-08-09 19:51:43 -0700

    programmatic new posts creation

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,25 @@
 
 // Specifying layout - buttons, titles, etc.
 
+// v-bind allows us to connect (bind) data (model) to a component
+// value is a standard attribute of any input
+// But component is not connected to data (model) yet (uni-directional)
+// Thus, changes in component (entering text in the input field) will not modify the data (model)
+
+// To make this connection bi-directional (syncronized) we need to subscribe to the 'input' event: @input
+// We can either pass a method name to @input (like we do for title), or pass the command directly (like we do for body)
+
+// @submit.prevent in the form prevents browser from refresh after we click the 'Add Post' button.
+// If a browser refreshes, we will loose the post that we've added using the form
 <template>
   <div>
+
+    <form @submit.prevent>
+      <h4>Post Creation</h4>
+      <input v-bind:value='title' @input='inputTitle' type='text' placeholder='Name'>
+      <input v-bind:value='body' @input='body = $event.target.value' type='text' placeholder='Description'>
+      <button @click='createPost'>Add Post</button>
+    </form>
 
     <div class="post" v-for="post in posts">
       <div><strong>Name: </strong> {{ post.title }}</div>
@@ -15,6 +32,10 @@
 </template>
 
 
+<!--
+newPost object should have a unique id
+We set this.title and this.body to empty strings after pushing new post to this.posts to erase the content of input forms
+-->
 <script>
 export default {
 
@@ -24,9 +45,29 @@ export default {
         {id: 1, title: 'Some Title', body: 'Some Description'},
         {id: 2, title: 'Some Title 2', body: 'Some Description 2'},
         {id: 3, title: 'Some Title 3', body: 'Some Description 3'}
-      ]
+      ],
+      title: '',
+      body: ''
     }
   },
+
+
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body,
+      }
+      this.posts.push(newPost)
+      this.title = '',
+      this.body = ''
+    },
+
+    inputTitle(event) {
+      this.title = event.target.value;
+    }
+  }
 
 }
 </script>
