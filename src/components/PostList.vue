1e9a43e8d25cd05f2b1de05c25ecb5ef1020e9ee
victor <victorvtf@gmail.com>
2021-08-10 18:29:23 -0700

    PostItem decomposition added

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,14 +1,19 @@
 <template>
-  <div class="post" v-for="post in posts">
-    <div><strong>Name: </strong> {{ post.title }}</div>
-    <div><strong>Description: </strong> {{ post.body }}</div>
+  <div>
+    <h3>Users List</h3>
+    <post-item
+      v-for="post in posts"
+      :post='post'
+    />
   </div>
 </template>
 
 <!--props defines arguments of the current component (posts in our case)
 that are passed from a parent (App.vue in our case)-->
 <script>
+import PostItem from '@/components/PostItem';
 export default {
+  components: {PostItem},
   props: {
     posts: {
       type: Array,
